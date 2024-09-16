d674edc78545943489b5525d38a1c0de30e509d1
Roman Kuvshanov <roman.kuvshanov.work@gmail.com>
2024-09-16 09:16:29 +0500

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import RememberPassword from "./pages/RememberPassword/RememberPassword";
+import Logout from "./pages/Logout/Logout";
+import Personal from "./pages/Personal/Personal";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/rememberpassword" element={<RememberPassword />} />
+      <Route path="/personal" element={<Personal />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="*" element={<Navigate to={"/signin"} />} />
+    </Routes>
   );
 }
 
